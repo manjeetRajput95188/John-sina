0c278e8ededbd574c91e2246bb74ba24ecfd2024
John Sina <johnsina@gmail.com>
2025-02-09 18:40:00 +0530

    second commjt

---
+++ helloWorld.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    cout<<"Hello World"<<endl;
+    cout<<"This is the second line"<<endl;
+    cout<<"configration"<<endl;
+    return 0;
+}
